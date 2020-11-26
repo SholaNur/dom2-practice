292e399445d1e7c031410392acaddc4671306365
SholaNur <shoola.nur@gmail.com>
2020-11-25 19:49:50 -0800

    DOM practice

---
+++ app.js
@@ -0,0 +1,6 @@
+let order = document.getElementsByTagName("li");
+console.log(order);
+order[1].style.fontStyle = "italic";
+order[2].style.color = "red";
+document.querySelector("button").style.backgroundColor = "lightblue";
+document.querySelector("li a").style.color = "orange";
