50ca4971851751f4fc6ed70d19c062a32a4b4d14
SholaNur <shoola.nur@gmail.com>
2020-11-25 20:52:31 -0800

    added new property and method

--- app.js
+++ app.js
@@ -4,3 +4,6 @@ order[1].style.fontStyle = "italic";
 order[2].style.color = "red";
 document.querySelector("button").style.backgroundColor = "lightblue";
 document.querySelector("li a").style.color = "orange";
+
+// classList => property and add, remove, toggle => methods
+document.querySelector("button").classList.add("huge");
